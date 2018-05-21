d4bc27c73a09bfd4fd8319ed87eba2cbdb2bea46
Peter Krautzberger <p.krautzberger@gmail.com>
2018-05-21 16:52:22 +0200

    rename doc => docs

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ for dir in $(find ./src/* -maxdepth 0 -type d)
 do
     cd "$dir"
     npx remarker build
-    mkdir -p "../../doc/${dir##*/}"
-    cp -r build/* "../../doc/${dir##*/}/."
-    cd ..
+    mkdir -p "../../docs/${dir##*/}"
+    cp -r build/* "../../docs/${dir##*/}/."
+    cd ../..
 done
